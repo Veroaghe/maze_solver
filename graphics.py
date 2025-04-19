548cd3a3d8faa33d0a9c749a2212cbd765ad33eb
Glenn Verhaeghe <veroaghe@gmail.com>
2025-04-19 02:50:10 +0200

    Completed Ch1L4 - Point and Line class added; draws lines in the Window

--- graphics.py
+++ graphics.py
@@ -3,14 +3,14 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
         self.__root = Tk()
         self.__root.title = "Maze Solver"
 
-        self.__cvs = Canvas(self.__root, width=self.__width, height=self.__height)
-        self.__cvs.pack()
+        self.__cvs = Canvas(self.__root, bg="white", width=self.width, height=self.height)
+        self.__cvs.pack(fill=BOTH, expand=1)
 
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -18,6 +18,10 @@ class Window:
     def redraw(self):
         self.__root.update()
         self.__root.update_idletasks()
+
+    def draw_line(self, line, fill_color="black"):
+        '''line is an instance of Line'''
+        line.draw(self.__cvs, fill_color)
     
     def wait_for_close(self):
         self.__running = True
@@ -27,3 +31,25 @@ class Window:
     
     def close(self):
         self.__running = False
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        ''' Takes 2 instances of Point as its arguments'''
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, cvs, fill_color):
+        x1 = self.p1.x
+        y1 = self.p1.y
+        x2 = self.p2.x
+        y2 = self.p2.y
+        cvs.create_line(
+            x1, y1, x2, y2, fill=fill_color, width=2
+        )
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from graphics import Window
+from random import randrange
+from graphics import Window, Point, Line
 
 
 def main():
     win = Window(800, 600)
+
+    for _ in range(3):
+        p1 = Point(randrange(0, win.width), randrange(0, win.height))
+        p2 = Point(randrange(0, win.width), randrange(0, win.height))
+        line = Line(p1, p2)
+        win.draw_line(line, "black")
+
     win.wait_for_close()
 
 
